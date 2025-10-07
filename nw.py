0d6595f0afb771fed9b53341dccfa6e1c8bc0cdf
Ben Isaacs <isaacsb294@gmail.com>
2025-10-07 23:16:36 +0100

    new file (py or c) with def main/int main ready printed (for making new utils lol)

---
+++ nw.py
@@ -0,0 +1,42 @@
+py_content = """def main():
+    pass
+
+
+if __name__ == \"__main__\":
+    main()
+
+    """
+
+c_content = """int main(int argc, char *argv[]) {
+    return 0;
+}"""
+
+
+def get_content(extension: str):
+    match extension:
+        case "py":
+            return py_content
+        case "c":
+            return c_content
+        case _:
+            return ""
+
+
+def main(filename: str, extension: str):
+    content = get_content(extension)
+
+    with open(f"{filename}.{extension}", "w", encoding="utf-8") as f:
+        rc: int = f.write(content)
+        if rc > 0:
+            print("done")
+
+
+if __name__ == "__main__":
+    import sys
+    import os
+
+    filename = sys.argv[1]
+    extension = sys.argv[2]
+
+    main(filename, extension)
+    os.system(f"nvim {filename}.{extension}")
